1992ded55ed7076960327ca744da74c0e8a757c1
mattpe <mattpe@metropolia.fi>
2026-01-21 10:18:40 +0200

    add state & conditional rendering demo; dummy login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 //import './App.css';
+import Greeting from './components/Greeting';
 import Home from './components/Home';
 
 const App = () => {
   return (
     <>
       <h1>My Media sharing app</h1>
+      <Greeting />
       <Home />
     </>
   );
