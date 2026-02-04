2ffc31357a8659afafb2675db1e44bfb077732c0
mattpe <mattpe@metropolia.fi>
2026-02-04 10:21:18 +0200

    add upload form

--- src/views/Upload.tsx
+++ src/views/Upload.tsx
@@ -1,20 +1,78 @@
 import {useState} from 'react';
+import useForm from '../hooks/formHooks';
 
 const Upload = () => {
-  const [uploading, setUploading] = useState(false);
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [file, setFile] = useState<File | null>(null);
+
+  const initValues = {title: '', description: ''};
+  const doUpload = () => {
+    setUploading(true);
+    // TODO: replace following with real upload functionality
+    setTimeout(() => {
+      setUploading(false);
+    }, 3000);
+  };
+  const {handleInputChange, handleSubmit, inputs} = useForm(
+    doUpload,
+    initValues,
+  );
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log(event.target.files);
+    if (event.target.files) {
+      setFile(event.target.files[0]);
+    }
+  };
+
   return (
     <>
-      <h2>Upload</h2>
-      <button
-        onClick={() => {
-          setUploading(true);
-          setTimeout(() => {
-            setUploading(false);
-          }, 3000);
-        }}
-      >
-        Upload
-      </button>
+      <h1>Upload</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            name="title"
+            type="text"
+            id="title"
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            rows={5}
+            id="description"
+            onChange={handleInputChange}
+          ></textarea>
+        </div>
+        <div>
+          <label htmlFor="file">File</label>
+          <input
+            name="file"
+            type="file"
+            id="file"
+            accept="image/*, video/*"
+            onChange={handleFileChange}
+          />
+        </div>
+        <img
+          src={
+            file
+              ? URL.createObjectURL(file)
+              : 'https://placehold.co/320x240?text=Choose+image'
+          }
+          alt="preview"
+          width="200"
+        />
+        <button
+          type="submit"
+          disabled={file && inputs.title.length > 3 ? false : true}
+        >
+          Upload
+        </button>
+      </form>
       {uploading && <p>Uploading...</p>}
     </>
   );
