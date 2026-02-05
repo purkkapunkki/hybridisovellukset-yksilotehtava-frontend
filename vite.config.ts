776a918f6d658ce2b00be4aba00662e1d577f422
purkkapunkki <120955113+purkkapunkki@users.noreply.github.com>
2026-02-05 21:09:43 +0200

    Fixed base URL

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/~nellili/hybrid26/tailwind/',
+  base: '/',
   test: {
     environment: 'jsdom',
     globals: true,
