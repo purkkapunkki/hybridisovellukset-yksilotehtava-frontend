f221119611112feb80e8840c67f688b47a932f23
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-02-10 15:00:42 +0200

    feat: implement like functionality in Likes component; add state management for like count and user like

--- src/components/Likes.tsx
+++ src/components/Likes.tsx
@@ -1,15 +1,86 @@
+import {useEffect, useReducer} from 'react';
 import Button from './Button';
-import type {MediaItemWithOwner} from 'hybrid-types/DBTypes';
+import type {Like, MediaItemWithOwner} from 'hybrid-types/DBTypes';
+import {useLike} from '../hooks/apiHooks';
 
 type LikesType = {
   item: MediaItemWithOwner | undefined;
 };
 
+type LikeState = {
+  count: number;
+  userLike: Like | null;
+};
+
+type LikeAction = {
+  type: 'setLikeCount' | 'like';
+  like?: Like | null;
+  count?: number;
+};
+
+const likeInitialState: LikeState = {
+  count: 0,
+  userLike: null,
+};
+
 const Likes = ({item}: LikesType) => {
+  const [likeState, likeDispatch] = useReducer(likeReducer, likeInitialState);
+
+  const {postLike, deleteLike, getCountByMediaId, getUserLike} = useLike();
+
+  const getLikes = async () => {
+    const token = localStorage.getItem('token');
+
+    if (!item || !token) {
+      return;
+    }
+
+    try {
+      const userLike = await getUserLike(item.media_id, token);
+      likeDispatch({type: 'like', like: userLike});
+    } catch (error) {
+      likeDispatch({type: 'like', like: null});
+      console.log('get user like error', (error as Error).message);
+    }
+  };
+
+  const getLikeCount = async () => {
+    // TODO: get like count and dispatch it to the state
+    if (!item) {
+      return;
+    }
+
+    try {
+      const {count} = await getCountByMediaId(item.media_id);
+      likeDispatch({type: 'setLikeCount', count});
+    } catch (error) {
+      console.log('get user like error', (error as Error).message);
+    }
+  };
+
+  useEffect(() => {
+    getLikes();
+    getLikeCount();
+  }, []);
+
+  function likeReducer(state: LikeState, action: LikeAction): LikeState {
+    switch (action.type) {
+      case 'setLikeCount':
+        return {...state, count: action.count ?? 0};
+      case 'like':
+        if (action.like !== undefined) {
+          return {...state, userLike: action.like};
+        }
+        return state; // no change if action.like is undefined
+      default:
+        return state; // Return the unchanged state if the action type is not recognized
+    }
+  }
+
   return (
     <>
       <Button value="Like" />
-      <p>Likes: 3</p>
+      <p>Likes: {likeState.count}</p>
     </>
   );
 };
