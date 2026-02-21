f0916c123530ed559bb15dc6fcd618a4f16be9f2
purkkapunkki <120955113+purkkapunkki@users.noreply.github.com>
2026-02-21 22:17:47 +0200

    Created an Input component
    This way it's easier to create input elements with a consistent style.

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,9 @@
+import Input from './ui/input';
+
 const SearchBar = () => {
   return (
     <div className="flex items-center justify-center">
-      <input
-        type="text"
-        placeholder="Search with tags..."
-        className="rounded-l-md border border-violet-300 px-4 py-2 focus:border-pink-500 focus:outline-none"
-      />
+      <Input placeholder="Search with tags..." className="rounded-l-md"></Input>
       <button className="rounded-r-md bg-violet-500 from-indigo-500 via-purple-500 to-pink-500 px-4 py-2 text-white hover:bg-gradient-to-r focus:outline-none">
         Search
       </button>
