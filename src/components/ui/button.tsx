1dfc025f67c8a7f2d0607607a82cea9f994428d7
purkkapunkki <120955113+purkkapunkki@users.noreply.github.com>
2026-02-22 18:21:49 +0200

    Updated the default button variant's style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default:
+          'bg-violet-500 from-indigo-500 via-purple-500 to-pink-500 text-white hover:bg-gradient-to-r focus:outline-none',
         destructive:
           'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
