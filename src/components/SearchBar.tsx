7c2a1f64a5c06a012bf5357a6d2550c5e18300b4
purkkapunkki <120955113+purkkapunkki@users.noreply.github.com>
2026-03-07 20:35:13 +0200

    Changed the search button's appearance

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -4,7 +4,7 @@ const SearchBar = () => {
   return (
     <div className="flex items-center justify-center">
       <Input placeholder="Search with tags..." className="rounded-l-md"></Input>
-      <button className="rounded-r-md bg-violet-500 from-indigo-500 via-purple-500 to-pink-500 px-4 py-2 text-white hover:bg-gradient-to-r focus:outline-none">
+      <button className="bg-midpurple from-midpurple to-lightpink rounded-r-md px-4 py-2 text-white hover:bg-gradient-to-r focus:outline-none">
         Search
       </button>
     </div>
