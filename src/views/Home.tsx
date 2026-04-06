2d9353f9021d1142700da620d9b120678c3c9986
purkkapunkki <120955113+purkkapunkki@users.noreply.github.com>
2026-04-06 12:33:23 +0300

    Changed background colors

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -2,19 +2,20 @@ import type {MediaItemWithOwner} from 'hybrid-types/DBTypes';
 import {useState} from 'react';
 import MediaRow from '../components/MediaRow';
 import SingleView from '../components/SingleView';
-import FollowedList from '../components/FollowedList';
+import FollowedList from '../components/NewPostsList';
 import PopularTagsList from '../components/PopularTagsList';
 import Footer from '../components/Footer';
 import {useMedia} from '../hooks/apiHooks';
 import {useUserContext} from '@/hooks/ContextHooks';
 
 const Home = () => {
+  const token = localStorage.getItem('token') || '';
   const [selectedItem, setSelectedItem] = useState<
     MediaItemWithOwner | undefined
   >(undefined);
 
   const {user} = useUserContext();
-  const {mediaArray, setMediaArray} = useMedia();
+  const {mediaArray, setMediaArray} = useMedia(token);
 
   if (user === null) {
     return '';
@@ -30,7 +31,7 @@ const Home = () => {
       {selectedItem && (
         <SingleView item={selectedItem} setSelectedItem={setSelectedItem} />
       )}
-      <div className="bg-darkpurple mr-5 ml-5 flex space-x-4">
+      <div className="mr-5 ml-5 flex space-x-4 bg-white">
         <section className="flex-1">
           <PopularTagsList />
         </section>
